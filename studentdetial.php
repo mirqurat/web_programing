cc47e93be294645d7515d9e67f625065ef8652a8
QURAT-UL-EIN <112850118+mirqurat@users.noreply.github.com>
2024-06-04 21:11:10 +0530

    inserting data in database using php

---
+++ studentdetial.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <h2>this is student detail form</h2>
+    <form action="insert.php" method="post">
+        Enter name: <input type="text" name="name" ><br>
+        Enter roll-no: <input type="number" name="roll-no" ><br>
+        Enter address: <input type="text" name="address" ><br>
+        Enter course: <input type="text" name="course" ><br>
+        Enter DOB: <input type="number" name="DOB" ><br>
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
