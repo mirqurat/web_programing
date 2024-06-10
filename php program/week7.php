87f580da5ad8d699bbb98e9086f68a03a91adc1c
QURAT-UL-EIN <112850118+mirqurat@users.noreply.github.com>
2024-06-10 21:47:53 +0530

    1

---
+++ php program/week7.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST">
+        <input type="text" placeholder="name" name="name">
+        <br>
+        <input type="text" placeholder="address" name="reg">
+        <br>
+        <button>submit</button>
+    </form>
+</body>
+<?php
+echo "assignment is"
+?>
+</html>
